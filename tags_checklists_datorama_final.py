756dc4a02960ad78f4021dafb91a658b9024d394
Shane Bacchus <sbacchus91@gmail.com>
2021-05-16 13:29:46 -0400

    Add files via upload

---
+++ tags_checklists_datorama_final.py
@@ -0,0 +1,39 @@
+import datorama
+import requests
+import pandas as pd
+import json
+
+headers = {
+    'X-API-KEY': 'ADD API KEY HERE',
+}
+
+templates_response = requests.get('https://public-api.process.st/templates', headers=headers, params={'limit':500})
+templates_response_json = templates_response.json()
+templates_df = pd.DataFrame(templates_response_json['data'])
+id_list = templates_df["id"].tolist()
+
+
+tags = []
+for x in id_list:
+    API_URL = "https://app.process.st/api/1/tag-memberships?templateId={}&v=4".format(x)
+    tag_response = requests.get(API_URL, auth=('ADD API KEY HERE', ''))
+    tag_response_json = tag_response.json()
+    tags.append(tag_response_json)
+
+res = [ele for ele in tags if ele != []]
+
+a_list = []
+b_list = []
+for x in res:
+    for line in x:
+        a_list.append(line['tag']['name'])
+        b_list.append(line['template']['id'])
+
+df = pd.DataFrame()
+df['Template_ID']  = b_list
+df['Tag_Name'] = a_list
+
+
+
+csv_string = df.to_csv()
+datorama.save_csv(csv_string)
